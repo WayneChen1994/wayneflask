d80a2d2fb7c030a16297d09a55b56cdb03ae1d3c
WayneChen1994 <waynechen1994@163.com>
2018-10-06 00:27:39 +0800

    201810060027

--- demos/form/app.py
+++ demos/form/app.py
@@ -5,6 +5,7 @@
 
 import os
 from flask import Flask, render_template, url_for
+from forms import LoginForm
 
 
 app = Flask(__name__)
@@ -13,6 +14,18 @@ app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
 
 
-@app.route('/html', methods=['GET', 'POST'])
-def html():
-    pass
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/basic', methods=['GET', 'POST'])
+def basic():
+    form = LoginForm()
+    return render_template('basic.html', form=form)
+
+
+@app.route('/bootstrap', methods=['GET', 'POST'])
+def bootstrap():
+    form = LoginForm()
+    return render_template('bootstrap.html', form=form)
