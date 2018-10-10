19c9e69a764559a0fc63f1e746f75b3375c3970f
WayneChen1994 <waynechen1994@163.com>
2018-10-10 21:03:46 +0800

    201810102103

--- demos/database/app.py
+++ demos/database/app.py
@@ -5,18 +5,35 @@
 
 import os
 import click
-from flask import Flask
+from flask import Flask, flash, redirect, url_for, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField, SubmitField
+from wtforms.validators import DataRequired
 
 
 app = Flask(__name__)
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:////' +
-                                                  os.path.join(app.root_path, 'data.db'))
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:////' + os.path.join(app.root_path, 'data.db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'a hard guess string')
+
 
 db = SQLAlchemy(app)
+
+
+class NewNoteForm(FlaskForm):
+    body = TextAreaField('Body', validators=[DataRequired()])
+    submit = SubmitField('Save')
+
+
+class EditNoteForm(NewNoteForm):
+    submit = SubmitField('Update')
+
+
+class DeleteNoteForm(FlaskForm):
+    submit = SubmitField('Delete')
 
 
 class Note(db.Model):
@@ -31,3 +48,36 @@ class Note(db.Model):
 def initdb():
     db.create_all()
     click.echo('Initialized database.')
+
+
+@app.route('/')
+def index():
+    form = DeleteNoteForm()
+    notes = Note.query.all()
+    return render_template('index.html', notes=notes, form=form)
+
+
+@app.route('/new', methods=['GET', 'POST'])
+def new_note():
+    form = NewNoteForm()
+    if form.validate_on_submit():
+        body = form.body.data
+        note = Note(body=body)
+        db.session.add(note)
+        db.session.commit()
+        flash('Your note is saved.')
+        return redirect(url_for('index'))
+    return render_template('new_note.html', form=form)
+
+
+@app.route('/edit/<int:note_id>', methods=['GET', 'POST'])
+def edit_note(note_id):
+    form = EditNoteForm()
+    note = Note.query.get(note_id)
+    if form.validate_on_submit():
+        note.body = form.body.data
+        db.session.commit()
+        flash('Your note is updated.')
+        return redirect(url_for('index'))
+    form.body.data = note.body
+    return render_template('edit_note.html', form=form)
